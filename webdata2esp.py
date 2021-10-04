2bdfba35172e2526f84c78ac8c16cc66e843e6ba
shyzik93 <shyzik93@mail.ru>
2021-10-04 10:09:23 +0300

    added standart temp path; showing file size

--- webdata2esp.py
+++ webdata2esp.py
@@ -13,6 +13,7 @@ import min_html
 import min_css
 import min_js
 
+TEMP_DIRECTORY = os.path.join(os.path.expandvars('%TEMP%'), 'webdata2esp')
 CONSTANTS_INO_BODY_BEFORE_BYTES = 'const char const_{func_name}[{fsize_in}] PROGMEM = {{'
 CONSTANTS_INO_BODY_AFTER_BYTES = '};\r\n'
 SET_HANDLERS_INO_HEAD = 'void set_handlers(void) {{\r\n'
@@ -50,7 +51,10 @@ def get_context(lang, path, x=0, context=None):
     return context
 
 
-def transform(path_webpage, path_set_handlers, path_constants, fnames, temp_path, input_path, language):
+def transform(path_webpage, path_set_handlers, path_constants, fnames, input_path, language):
+    if not os.path.exists(TEMP_DIRECTORY):
+        os.mkdir(TEMP_DIRECTORY)
+
     context = get_context(
         language,
         os.path.join(input_path, 'languages'),
@@ -65,45 +69,48 @@ def transform(path_webpage, path_set_handlers, path_constants, fnames, temp_path
         for fname_in in fnames:
             print('file:', fname_in)
             fpath_in = os.path.join(input_path, fname_in)
-            fpath_in_min = os.path.join(temp_path, fname_in)
-            os.system('mkdir -p "{}" && cp "{fpath_in}" "{fpath_in_min}"'.format(
+            fpath_in_min = os.path.join(TEMP_DIRECTORY, fname_in)
+            os.system('mkdir -p "{}" && cp "{fpath_in}" "{fpath_in_min}"'.format(  # TODO: заменить Bash на Python
                 os.path.dirname(fpath_in_min),
                 fpath_in=fpath_in,
                 fpath_in_min=fpath_in_min
             ))
+            fsize = os.path.getsize(fpath_in)
             fpath_in = fpath_in_min
-            print('  compilation of template...')
+            print('    SIZE: {}\n  compilation of template...'.format(fsize))
             template = env.get_template(fname_in)
             with open(fpath_in, 'w', encoding='utf-8') as f:
                 f.write(template.render(context))
 
-            print('  minification...')
+            fsize = os.path.getsize(fpath_in)
+            print('    SIZE: {}\n  minification...'.format(fsize))
             if fname_in.endswith('.html'):
-                mHTML.min(fpath_in, fpath_in_min, fnames)
+                mHTML.min(fpath_in, fpath_in, fnames)
             elif fname_in.endswith('.css'):
-                mCSS.min(fpath_in, fpath_in_min)
-            # elif fname_in.endswith('.js'):
-            #     mJS.min(fpath_in, fpath_in_min)
+                mCSS.min(fpath_in, fpath_in)
+            elif fname_in.endswith('.js'):
+                mJS.min(fpath_in, fpath_in_min)
 
-            print('  archiving...')
+            fsize = os.path.getsize(fpath_in)
+            print('    SIZE: {}\n  archiving...'.format(fsize))
             gz_fpath_in = '{}.gz'.format(fpath_in)
             with open(gz_fpath_in, 'wb') as myzip, open(fpath_in, 'rb') as s:
                 myzip.write(gzip.compress(s.read()))
 
-            print('  converting into C-code for Arduino...')
+            fsize = os.path.getsize(gz_fpath_in)
+            print('    SIZE: {}\n  converting into C-code for Arduino...'.format(fsize))
             fmtype = mimetypes.guess_type(fname_in)
-            fsize_in = os.path.getsize(gz_fpath_in)
             func_name = 'handler_{}'.format(fname_in.replace('.', '_').replace('/', '_'))
             f_out.write(WEBPAGE_INO_BODY.format(
                 func_name=func_name,
                 fmtype=fmtype[0] if fmtype[0] else 'text/plain',
-                fsize_in=fsize_in,
+                fsize_in=fsize,
             ))
             f_out2.write(SET_HANDLERS_INO_BODY.format(fname_in=fname_in, func_name=func_name))
-            f_out3.write(CONSTANTS_INO_BODY_BEFORE_BYTES.format(func_name=func_name, fsize_in=fsize_in))
+            f_out3.write(CONSTANTS_INO_BODY_BEFORE_BYTES.format(func_name=func_name, fsize_in=fsize))
             with open(gz_fpath_in, 'rb') as f_in:
-                for i in range(1, fsize_in):
-                    f_out3.write('{}{}'.format(f_in.read(1)[0], ',' if i < fsize_in-1 else ''))
+                for i in range(1, fsize):
+                    f_out3.write('{}{}'.format(f_in.read(1)[0], ',' if i < fsize-1 else ''))
                     f_in.seek(i)
 
             f_out3.write(CONSTANTS_INO_BODY_AFTER_BYTES)
@@ -129,16 +136,12 @@ if __name__ == '__main__':
     cli_args = cli_parser.parse_args()
     input_path = os.path.expanduser(os.path.normpath(cli_args.input_path))
     output_path = os.path.expanduser(os.path.normpath(cli_args.output_path))
-    temp_path = os.path.join(os.getcwd(), 'temp')
-    if not os.path.exists(temp_path):
-        os.mkdir(temp_path)
 
     transform(
         os.path.join(output_path, 'webpage.ino'),
         os.path.join(output_path, 'set_handlers.ino'),
         os.path.join(output_path, 'constants.ino'),
         cli_args.files,
-        temp_path,
         input_path,
         cli_args.lang.upper()
     )
